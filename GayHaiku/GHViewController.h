7f64e8f073a3b91e1816b13af1bc61178e6ff720
Joel Derfner <joel@joelderfner.com>
2012-08-11 01:27:30 -0400

    Getting closer.

--- GayHaiku/GHViewController.h
+++ GayHaiku/GHViewController.h
@@ -8,14 +8,13 @@
 //  Note:  Layout is provisional until I get a graphic that isn't under copyright.  The new graphic is expected to run along the bottom of the screen, leaving more room for the haiku themselves (and allowing them to appear in a larger font).
 
 #import <UIKit/UIKit.h>
-#import "GHWebViewController.h"
 #import <QuartzCore/QuartzCore.h>
 
 @interface GHViewController : UIViewController <UITextViewDelegate,UIAlertViewDelegate>
 {
     IBOutlet UINavigationBar *navBarForDone;
     IBOutlet UITextView *haiku_text;
-    IBOutlet UIWebView *wView;
+    IBOutlet UINavigationBar *navBarForAmazon;
 }
 
 
@@ -25,12 +24,19 @@
 @property (nonatomic, retain) UITextView *haiku_text;
 @property (nonatomic, retain) NSMutableArray *theseAreDone;
 @property (nonatomic) int indx;
+@property (nonatomic, retain) UITextView *textView;
+@property (nonatomic, retain) UIWebView *webV;
 
 -(IBAction)chooseDatabase:(UISegmentedControl *)sender;
 -(IBAction)nextHaiku;
 -(IBAction)previousHaiku;
 -(IBAction)showMessage:(int)sender;
-- (IBAction)loadAmazon;
+-(IBAction)loadAmazon;
 -(IBAction)userWritesHaiku;
+-(IBAction)userFinishedWritingHaiku;
+-(IBAction)webBack;
+-(IBAction)doneWithAmazon;
+-(IBAction)haikuInstructions;
+
 
 @end
--- GayHaiku/GHViewController.m
+++ GayHaiku/GHViewController.m
@@ -20,20 +20,116 @@
 @synthesize gayHaiku = _gayHaiku;
 @synthesize haiku_text = _haiku_text;
 @synthesize selectedCategory = _selectedCategory;
-@synthesize wView = _wView;
+@synthesize webV = _webV;
 @synthesize theseAreDone;
 @synthesize indx;
+@synthesize textView;
 
+-(IBAction)haikuInstructions
+{
+    
+}
 
+-(IBAction)loadAmazon
+{    
+    self.webV.delegate = self;
+    self.webV = [[UIWebView alloc] init];
+    NSString *fullURL=@"http://www.amazon.com/Books-by-Joel-Derfner/lm/RVZNXKV59PL51/ref=cm_lm_byauthor_full";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    self.webV.scalesPageToFit=YES;
+    [self.webV loadRequest:requestObj];
+    [self.webV setFrame:(CGRectMake(0,44,320,372))];
+    [self.view addSubview:self.webV];
+    [self.view viewWithTag:80].hidden=NO;
+    /*
+     Still to do:
+     1.  Prevent back button (tag:90) from appearing until user has clicked a link.
+     2.  Move title to right when back button appears.
+     */
+}
+
+-(IBAction)doneWithAmazon
+{
+    [self.webV removeFromSuperview];
+    [self.view viewWithTag:80].hidden=YES;
+}
+
+- (IBAction)haikuInstructions:(id)sender {
+}
+
+-(IBAction)webBack
+{
+    if (self.webV.canGoBack)
+    {
+        [self.webV goBack];
+    }
+}
 
 -(IBAction)userWritesHaiku
 {
     [self.view viewWithTag:50].hidden=NO;
     [self.view viewWithTag:60].hidden=NO;
-    //Load keyboard.
-    //Give user option to get instructions on how to write a haiku.
-    //Give user option to opt out of sending any haiku s/he composes to my central database.
-    //Let user compose haiku, edit, save to plist under category USER.
+    self.textView = (UITextView *)[self.view viewWithTag:50];
+    self.textView.delegate = self;
+    self.textView.returnKeyType = UIReturnKeyDefault;
+    self.textView.keyboardType = UIKeyboardTypeDefault;
+    self.textView.scrollEnabled = YES;
+    self.textView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview: self.textView];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+
+    /*
+     Still to do:
+     1.  Give user option to get instructions on how to write a haiku.
+     2.  Give user option to opt out of sending any haiku s/he composes to my central database.
+     3.  Let user compose haiku, edit, save to plist under category USER.
+     4.  Connect to CMU pronunciation dictionary and set up method whereby, if haiku is done incorrectly (wrong number of lines, wrong number of syllables in a line) user is alerted and given the option to go back and edit.
+     5.  Create new method showing user a preview of new haiku and asking whether to a) save or b) edit.
+     6.  Animate appearance of title bar?
+     */
+}
+
+-(void)keyboardWillShow:(NSNotification *)aNotification
+{
+    CGRect keyboardRect = [[[aNotification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    NSTimeInterval animationDuration = [[[aNotification userInfo] objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    CGRect frame = self.view.frame;
+    frame.size.height -= keyboardRect.size.height;
+    [UIView beginAnimations:@"ResizeForKeyboard" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView commitAnimations];
+}
+
+
+-(IBAction)userFinishedWritingHaiku
+{
+        [self.view viewWithTag:50].hidden=YES;
+        [self.view viewWithTag:60].hidden=YES;
+        [self.textView resignFirstResponder];
+        [self.view viewWithTag:40].hidden=NO;
+}
+
+-(void)keyboardWillHide:(NSNotification *)aNotification
+{
+    NSTimeInterval animationDuration = [[[aNotification userInfo] objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    [UIView beginAnimations:@"ResizeForKeyboard" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView commitAnimations];
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView
+{
+    UIBarButtonItem* saveItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(saveAction:)];
+    self.navigationItem.rightBarButtonItem = saveItem;
+}
+
+- (void)saveAction
+{
+    [self.textView resignFirstResponder]; 
 }
 
 -(void)viewDidLoad {
@@ -43,16 +139,10 @@
 }
 
 -(void)viewDidUnload {
+    navBarForAmazon = nil;
 	[super viewDidUnload];
 	self.gayHaiku=nil;
 	self.haiku_text=nil;
-}
--(IBAction)loadAmazon
-{
-    //This UIWebView shit is making my head hurt.  I think the reason is that I'm using delegates wrong, but that could be totally incorrect.
-    [self.wView viewWithTag:70].hidden=NO;
-    GHWebViewController *wv;
-    [self.wView addSubview:wv];
 }
 
 - (IBAction)chooseDatabase:(UISegmentedControl *)segment {
@@ -138,7 +228,6 @@
         {
             self.indx=0;
         }
-        NSLog(@"About to do new haiku: %d",self.indx);
         NSString *cat = self.selectedCategory;
         //For now (adjust later so that, according to UISegmentedControl, it will also show only the user's haiku or all haiku):
         cat = @"Derfner";
@@ -159,13 +248,8 @@
                 {
                     self.theseAreDone = [[NSMutableArray alloc] init];
                 }
-                NSLog(@"About to add int to  array with %d",self.theseAreDone.count);
-                NSLog(@"self.indx is %d",self.indx);
                 [theseAreDone addObject:[filteredArray objectAtIndex:sortingHat]];
-                NSLog(@"Just added int to array for %d",self.theseAreDone.count);
                 self.indx = self.theseAreDone.count;
-                NSLog(@"self.indx is %d",self.indx);
-                NSLog(@"Just did new haiku: %d",self.indx);
             }
             else 
             {
@@ -177,13 +261,10 @@
 
 -(IBAction)previousHaiku
 {
-    if (self.theseAreDone.count>0)
+    if (self.theseAreDone.count>0 && self.indx>0)
     {
-    //Why is self.indx 0 here no matter how many new haiku you've done (and so no matter what number self.indx is after newHaiku)?
-    NSLog(@"About to do previous haiku: %d",self.indx);
     self.indx -= 1;
     self.haiku_text.text = [[self.theseAreDone objectAtIndex:self.indx-1] valueForKey:@"quote"];
-    NSLog(@"Just did previous haiku: %d",self.indx);
     }
 }
 
